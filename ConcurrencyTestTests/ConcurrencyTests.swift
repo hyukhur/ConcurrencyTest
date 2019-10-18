b984c2317a3b46af1721112db03f19404e686286
Hyuk Hur <hyukhur@gmail.com>
2019-10-17 23:23:46 -0700

    Implements `loadMessage` func with Operation
    This function should fetch both parts of the message (concurrently using GCD or OperationQueue) and join them with
    a space. e.g if `fetchMessageOne` completes with "Good" and `fetchMessageTwo` completes with "morning!" then loadMessage should call it's completion once with the String:
      "Good morning!"
    If loading either part of the message takes more than 2 seconds then `loadMessage` should complete with the String
      "Unable to load message - Time out exceeded"
    The function should only complete once and must always return the full message in the correct order.

--- ConcurrencyTestTests/ConcurrencyTests.swift
+++ ConcurrencyTestTests/ConcurrencyTests.swift
@@ -8,8 +8,60 @@ import XCTest
 @testable import ConcurrencyTest
 
 class ConcurrencyTests: XCTestCase {
+    static let expectMessage = "Hello world"
+    static let tryThreadhold = 100
+
     func testloadMessage() {
-        
+        let expectation = self.expectation(description: "testloadMessage")
+        loadMessage { combinedMessage in
+            XCTAssertTrue([ConcurrencyTests.expectMessage, ConcurrencyTests.errorMessage].contains(combinedMessage), combinedMessage)
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 10)
     }
 
+    func testloadMessageWithinTimeout() {
+        var expected = ""
+        let group = DispatchGroup()
+
+        for _ in (0..<ConcurrencyTests.tryThreadhold) {
+            let start = Date()
+            var diff = TimeInterval(0)
+            group.enter()
+            loadMessage { combinedMessage in
+                expected = combinedMessage
+                diff = Date().timeIntervalSince(start)
+                group.leave()
+            }
+            group.wait()
+            if TimeInterval(2) >= diff {
+                break
+            }
+        }
+
+        XCTAssertEqual(ConcurrencyTests.expectMessage, expected)
+    }
+
+    static let errorMessage = "Unable to load message - Time out exceeded"
+    func testloadMessageTimeout() {
+        var expected = ""
+        let group = DispatchGroup()
+
+        for _ in (0..<ConcurrencyTests.tryThreadhold) {
+            let start = Date()
+            var diff = TimeInterval(0)
+            group.enter()
+            loadMessage { combinedMessage in
+                expected = combinedMessage
+                diff = Date().timeIntervalSince(start)
+                group.leave()
+            }
+            group.wait()
+            if TimeInterval(2) < diff {
+                break
+            }
+        }
+
+        XCTAssertEqual(ConcurrencyTests.errorMessage, expected)
+    }
 }
--- YourCode/Concurrency.swift
+++ YourCode/Concurrency.swift
@@ -4,12 +4,54 @@
 
 import Foundation
 
+class DelayFinishBlockOperation: BlockOperation {
+    override var isFinished: Bool {
+        return beFinished || isCancelled
+    }
+    var beFinished: Bool = false {
+        willSet {
+            willChangeValue(for: \.isFinished)
+        }
+        didSet {
+            didChangeValue(for: \.isFinished)
+        }
+    }
+    override class func automaticallyNotifiesObservers(forKey key: String) -> Bool {
+        return ["finished", "isFinished"].contains(key) == false
+    }
+}
+
+let timeoutTimeInterval:TimeInterval = 2.0
+let timeoutMessage = "Unable to load message - Time out exceeded"
+
 func loadMessage(completion: @escaping (String) -> Void) {
-    
-    fetchMessageOne { (messageOne) in
+    let start = Date()
+    var message = timeoutMessage
+    let timeout = timeoutTimeInterval
+    var resultOne = ""
+    let operationQueue = OperationQueue()
+    let operationOne = DelayFinishBlockOperation()
+    let operationTwo = DelayFinishBlockOperation()
+
+    operationOne.addExecutionBlock {
+        fetchMessageOne { (messageOne) in
+            resultOne = messageOne
+            operationOne.beFinished = true
+        }
     }
-    
-    fetchMessageTwo { (messageTwo) in
+    operationTwo.addExecutionBlock {
+        fetchMessageTwo { (messageTwo) in
+            let diff = Date().timeIntervalSince(start)
+            if diff <= timeout {
+                message = "\(resultOne) \(messageTwo)"
+            }
+            operationTwo.beFinished = true
+        }
     }
-    completion("Good morning!")
+
+    operationTwo.addDependency(operationOne)
+    operationQueue.addOperation(operationOne)
+    operationQueue.addOperation(operationTwo)
+    operationQueue.waitUntilAllOperationsAreFinished()
+    completion(message)
 }
