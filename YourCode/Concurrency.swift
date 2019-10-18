f9cc3f25002ba23e5cd8f24b1e61cf26dbae2e1f
Hyuk Hur <hyukhur@gmail.com>
2019-10-17 23:44:32 -0700

    Implements `loadMessage` func with GCD
    Refactor this function to use idiomatic Swift code.
    Follow the apple Swift naming guidelines. If you choose you can abstract classes, structs, protocols, enums, generics etc.

--- YourCode/Concurrency.swift
+++ YourCode/Concurrency.swift
@@ -4,30 +4,7 @@
 
 import Foundation
 
-/// Completion of the exection is delayed until `beFinished` is true.
-fileprivate class DelayFinishBlockOperation: BlockOperation {
-    /// Depends on `beFinished` or `isCancelled` to flag finished
-    override var isFinished: Bool {
-        return beFinished || isCancelled
-    }
-
-    /// To present whether delayed execution is finished.
-    var beFinished: Bool = false {
-        willSet {
-            willChangeValue(for: \.isFinished)
-        }
-        didSet {
-            didChangeValue(for: \.isFinished)
-        }
-    }
-
-    /// To support `isFinished` KVO with `beFinished` property.
-    override class func automaticallyNotifiesObservers(forKey key: String) -> Bool {
-        return ["finished", "isFinished"].contains(key) == false
-    }
-}
-
-let timeoutTimeInterval:TimeInterval = 2.0
+let timeoutTimeInterval: Int = 2
 let timeoutMessage = "Unable to load message - Time out exceeded"
 
 /// This function should fetch both parts of the message, `fetchMessageOne` and `fetchMessageTwo`.
@@ -35,36 +12,26 @@ let timeoutMessage = "Unable to load message - Time out exceeded"
 ///   "Unable to load message - Time out exceeded".
 ///
 /// This function blocks another execution.
-/// If `fetchMessageTwo` doesn't execute the completion handler, it would be freezed.
 /// - Parameter completion: The completion handler to execute after the fetch all message is completed.
 func loadMessage(completion: @escaping (String) -> Void) {
-    let start = Date()
-    var message = timeoutMessage
-    let timeout = timeoutTimeInterval
-    var resultOne = ""
-    let operationQueue = OperationQueue()
-    let operationOne = DelayFinishBlockOperation()
-    let operationTwo = DelayFinishBlockOperation()
-
-    operationOne.addExecutionBlock {
-        fetchMessageOne { (messageOne) in
-            resultOne = messageOne
-            operationOne.beFinished = true
-        }
+    let group = DispatchGroup()
+    var firstMessage = ""
+    var secondMessage = ""
+    group.enter()
+    fetchMessageOne { (messageOne) in
+        firstMessage = messageOne
+        group.leave()
     }
-    operationTwo.addExecutionBlock {
-        fetchMessageTwo { (messageTwo) in
-            let diff = Date().timeIntervalSince(start)
-            if diff <= timeout {
-                message = "\(resultOne) \(messageTwo)"
-            }
-            operationTwo.beFinished = true
-        }
+    group.enter()
+    fetchMessageTwo { (messageTwo) in
+        secondMessage = messageTwo
+        group.leave()
     }
 
-    operationTwo.addDependency(operationOne)
-    operationQueue.addOperation(operationOne)
-    operationQueue.addOperation(operationTwo)
-    operationQueue.waitUntilAllOperationsAreFinished()
-    completion(message)
+    switch group.wait(timeout: DispatchTime.now() + DispatchTimeInterval.seconds(timeoutTimeInterval) ) {
+    case .success:
+        completion("\(firstMessage) \(secondMessage)")
+    case .timedOut:
+        completion(timeoutMessage)
+    }
 }
