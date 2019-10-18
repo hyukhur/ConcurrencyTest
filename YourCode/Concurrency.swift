a32f13efcf331d25411872f7a4ad0d77fcf644da
Hyuk Hur <hyukhur@gmail.com>
2019-10-17 23:40:05 -0700

    Comments a class and functions descriptions
    This comments are added to a class and functions to tell a notice which is the function blocks another execution.

--- YourCode/Concurrency.swift
+++ YourCode/Concurrency.swift
@@ -4,10 +4,14 @@
 
 import Foundation
 
-class DelayFinishBlockOperation: BlockOperation {
+/// Completion of the exection is delayed until `beFinished` is true.
+fileprivate class DelayFinishBlockOperation: BlockOperation {
+    /// Depends on `beFinished` or `isCancelled` to flag finished
     override var isFinished: Bool {
         return beFinished || isCancelled
     }
+
+    /// To present whether delayed execution is finished.
     var beFinished: Bool = false {
         willSet {
             willChangeValue(for: \.isFinished)
@@ -16,6 +20,8 @@ class DelayFinishBlockOperation: BlockOperation {
             didChangeValue(for: \.isFinished)
         }
     }
+
+    /// To support `isFinished` KVO with `beFinished` property.
     override class func automaticallyNotifiesObservers(forKey key: String) -> Bool {
         return ["finished", "isFinished"].contains(key) == false
     }
@@ -24,6 +30,13 @@ class DelayFinishBlockOperation: BlockOperation {
 let timeoutTimeInterval:TimeInterval = 2.0
 let timeoutMessage = "Unable to load message - Time out exceeded"
 
+/// This function should fetch both parts of the message, `fetchMessageOne` and `fetchMessageTwo`.
+/// If loading either part of the message takes more than 2 seconds then it should complete with the String
+///   "Unable to load message - Time out exceeded".
+///
+/// This function blocks another execution.
+/// If `fetchMessageTwo` doesn't execute the completion handler, it would be freezed.
+/// - Parameter completion: The completion handler to execute after the fetch all message is completed.
 func loadMessage(completion: @escaping (String) -> Void) {
     let start = Date()
     var message = timeoutMessage
